89a36751f9062f44b120e1eea98882562f4a962a
杨先林 <2421655760@qq.com>
2021-09-05 11:09:09 +0800

    2

--- bb/src/com/Hello.java
+++ bb/src/com/Hello.java
@@ -1,4 +1,9 @@
 package com;
 
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello  使劲儿");
+        System.out.println("hello  使劲儿");
+        System.out.println("hello  使劲儿");
+    }
 }
